ae075018d2776490784b2e5d4f83fa3a0710aa0b
rauf17 <connect2rauf17@gmail.com>
2026-05-10 15:49:48 +0500

    Chrono Rift Status : Completed

--- submission/renderer/renderer.cpp
+++ submission/renderer/renderer.cpp
@@ -5,6 +5,8 @@
 #include <algorithm>
 #include <semaphore.h>
 
+#pragma GCC diagnostic ignored "-Wunused-parameter"
+
 // ---------------------------------------------------------------------------
 // captureSnapshot
 // ---------------------------------------------------------------------------
